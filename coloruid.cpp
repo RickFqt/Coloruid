dcb70f1abe020b488d66c75b738f9a7508d53283
RickFqt <henriquefouquet@gmail.com>
2023-06-03 17:47:19 -0300

    Agora sim

--- coloruid.cpp
+++ coloruid.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 
 int main(){
-    // vertice: par de rótulo e cor (v, c)
-    // aresta: par de vertices (v1, v2)
+    // vertice: rotulados por numeros, que estará associado a uma cor por um (dicionário? é esse o nome?)
+    // aresta: par de vertices (v1, v2) (representada na lista de adjacência por 1 ou 0)
     // grafo: lista de arestas
 
 
@@ -25,11 +25,16 @@ int main(){
                 // Continue
         
 
-        // V = V - J
         // Condensar v com todos os vértices J:
-            // Para todo vértice i, i \in V/{v}, faça:
-                // Se i adjacente a j, para algum j \in J:
-                    // Tornar i adjacente a v; remover j da lista de adjacência de i
-        // v.cor() = j.cor()
+        
+            // V = V - J
+            // Para todo vértice i, i \in V\{v}, faça:
+                // Para todo vértice k, k \in J, faça:
+                    // Se adj(i, k) = 1:
+                        // adj(i, k) = adj(k, i) = 0; adj(i, v) = adj(v, i) = 1;
+            // Para todo vertice j, j \in J, faça:
+                // adj(j, v) = adj(v, j) = 0;
+            // Para algum vertice j \in J, faça:
+                // v.cor() = j.cor()
     return 0;
 }
